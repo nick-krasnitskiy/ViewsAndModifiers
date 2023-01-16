d6c10c24edd582e327a597f4665a525ed8753020
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-01-16 13:31:29 +0300

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Nick Krasnitskiy on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
