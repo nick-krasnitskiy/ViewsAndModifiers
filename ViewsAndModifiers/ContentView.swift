a13feec061c6acc5b1165213addbb19741f73ac4
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-01-16 17:19:04 +0300

    Use view composition.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,20 +7,28 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var motto1: some View {
-        Text("Draco dormiens")
-    }
-    
-    let motto2 = Text("nunquam titillandus")
-    
-    @ViewBuilder var spells: some View {
-        Text("Lumos")
-        Text("Obliviate")
-    }
+struct CapsuleText: View {
+    var text: String
     
     var body: some View {
-        spells
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .background(.blue)
+            .clipShape(Capsule())
+    }
+}
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+                .foregroundColor(.white)
+            CapsuleText(text: "Second")
+                .foregroundColor(.yellow)
+        }
+        
     }
 }
 
