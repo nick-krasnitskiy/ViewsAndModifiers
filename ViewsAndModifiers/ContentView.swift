fbf44a5ebaf417bc0689d5dbe20d0fba17c190a2
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-01-16 17:10:32 +0300

    Use views as properties.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,16 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    var motto1: some View {
+        Text("Draco dormiens")
+    }
+    
+    let motto2 = Text("nunquam titillandus")
+    
+    @ViewBuilder var spells: some View {
+        Text("Lumos")
+        Text("Obliviate")
+    }
     
     var body: some View {
-        VStack {
-            Text("Gryffindor")
-                .font(.largeTitle)
-            Text("Hufflepuff")
-            Text("Ravenclaw")
-            Text("Slytherin")
-        }
-        .font(.title)
+        spells
     }
 }
 
