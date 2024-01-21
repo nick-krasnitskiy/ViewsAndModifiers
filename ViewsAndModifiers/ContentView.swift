df103c7bd7fc8128d51e443ddb1a762ce7596905
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-01-21 23:11:20 +0300

    Use custom modifiers.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,24 +7,33 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        CapsuleText(text: "First")
-            .foregroundStyle(.white)
-        CapsuleText(text: "Second")
-            .foregroundStyle(.yellow)
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
+        }
     }
 }
 
-struct CapsuleText: View {
-    var text: String
-    
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+
+struct ContentView: View {
     var body: some View {
-        Text(text)
-            .font(.largeTitle)
-            .padding()
-            .background(.blue)
-            .clipShape(.capsule)
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
     }
 }
 
