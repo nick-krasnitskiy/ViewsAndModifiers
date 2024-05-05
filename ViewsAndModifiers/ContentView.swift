eba750ca903cea6ce7c82e34dc9bf80f63a29c2a
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-05-05 14:50:25 +0300

    Use environment modifiers.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var useRedText = false
-    
     var body: some View {
-        Button("Hello, world!") {
-            // flip the Boolean between true and false
-            useRedText.toggle()
+        VStack {
+            Text("Gryffindor")
+                .font(.largeTitle)
+            Text("Hufflepuff")
+            Text("Ravenclaw")
+            Text("Slytherin")
         }
-        .foregroundStyle(useRedText ? .red : .blue)
+        .font(.title)
     }
 }
 
